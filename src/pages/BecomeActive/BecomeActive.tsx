ede434b3885827d6bb45c60f05c31fa9ca2376fa
Jakob (Koby) Shimony <shimony@talon.one>
2020-03-22 17:35:09 +0100

    Add temporarily randomizing filtering

--- src/pages/BecomeActive/BecomeActive.tsx
+++ src/pages/BecomeActive/BecomeActive.tsx
@@ -6,19 +6,40 @@ import './BecomeActive.less'
 
 const BecomeActive = () => {
   const [results, setResults] = React.useState<any[]>([])
-  const [loading, setLoading] = React.useState(true)
-  React.useEffect(() => {
-    fetch('/api/activities')
+  const [loading, setLoading] = React.useState(false)
+  const fetchResults = () => {
+    setLoading(true)
+    return fetch('/api/activities')
       .then(res => res.json())
-      .then(data => setResults(data.projects))
+      .then(data => Promise.resolve(data.projects))
       .catch(console.log)
       .finally(() => setLoading(false))
+  }
+  const fakeSomeMagic = (clear?: boolean) => {
+    fetchResults()
+      .then(data => {
+        if (clear === true) {
+          return data
+        }
+
+        const randomized = []
+        const rand = Math.floor(Math.random() * data.length / 1.5)
+        for (let i = 0; i < rand; i++) {
+          randomized.push(data[i])
+        }
+        return randomized
+      })
+      .then(setResults)
+  }
+
+  React.useEffect(() => {
+    fetchResults().then(setResults)
   }, [])
 
   return (
     <Row gutter={24}>
       <Col span={6}>
-        <FiltersPanel />
+        <FiltersPanel onChange={fakeSomeMagic} />
       </Col>
       <Col span={18}>
         <ResultsList loading={loading} results={results} />
